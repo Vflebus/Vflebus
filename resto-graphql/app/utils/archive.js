6c4c8ce178d8324a8e661de45df32889e347e79b
Vincent Flebus <82810365+Vflebus@users.noreply.github.com>
2023-06-28 18:34:25 +0200

    Delete resto-graphql directory

--- resto-graphql/app/utils/archive.js
+++
@@ -1,8 +0,0 @@
-const { ArchiveRestaurant } = require('../models/archive');
-
-module.exports = (app) => {
-    app.on('restaurant:deleted', async (restaurant) => {
-        const literalRestaurant = restaurant.toJSON();
-        await ArchiveRestaurant.create(literalRestaurant);
-    });
-};
